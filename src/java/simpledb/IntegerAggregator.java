6f6cfcdb6005ba3fce6992ecb85a82e9fdaa4493
tang <876836336@qq.com>
2023-03-27 22:28:28 +0800

    Lab2 part of exercise2

--- src/java/simpledb/IntegerAggregator.java
+++ src/java/simpledb/IntegerAggregator.java
@@ -1,12 +1,35 @@
 package simpledb;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
+ * 
+ * 聚合（Aggregate）是指对表格中的数据进行计算并生成单个结果的操作。
+ * 通常情况下，聚合函数（Aggregate Function）会对一列或多列数据进行计算，生成汇总值。
+ * 
+ * 分组（Group）是指将数据按照某一或某几个列的值进行分类，然后对每个分类中的数据进行聚合计算。
+ * 这个操作会生成多个结果，每个结果对应一个或多个行，并按照分组列的值进行标识。
+ * 
  */
 public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
-
+    private int gbField;// index
+    private Type gbFieldType;
+    private int aField;// index
+    private Op operator;
+    
+//    private HashMap<Integer,Integer> group=new HashMap<>();// (groupValue,aggregateValue)!
+//    // 错了！麻烦了，你最后iterator返回的值还是field！groupValue只是schema :(
+//    private HashMap<Integer,Integer> avgCount=new HashMap<>();// 为了求平均值，记录每个组有多少元素
+    
+    private HashMap<Field,Integer> group=new HashMap<>();
+    private HashMap<Field,Integer> avgCount=new HashMap<>();
+    
     /**
      * Aggregate constructor
      * 
@@ -24,6 +47,10 @@ public class IntegerAggregator implements Aggregator {
 
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+    	this.gbField=gbfield;
+    	this.gbFieldType=gbfieldtype;
+    	this.aField=afield;
+    	this.operator=what;
     }
 
     /**
@@ -35,6 +62,62 @@ public class IntegerAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+    	// 对新加的一个tuple进行聚合操作，并进行分组
+    	// 先得到gbField和aField两个字段
+    	Field gb;
+    	//int gbValue=-1;
+    	if(this.gbField==Aggregator.NO_GROUPING)
+    		gb=null;
+    	else {
+    		gb=tup.getField(this.gbField);
+    		//gbValue=gbField.getValue();
+    	}
+    	
+    	IntField aField=(IntField) tup.getField(this.aField);
+    	int aValue=aField.getValue();
+    	
+    	// 对新加的这一行先判断是哪一组，再做aggregate
+    	switch(this.operator) {
+    	case MIN:
+    		if(!group.containsKey(gb))// 该组还没有东西
+    			group.put(gb, aValue);
+    		else// 组里有东西，执行一次aggregate
+    			group.put(gb, Math.min(aValue, group.get(gb)));
+    		break;
+    	case MAX:
+    		if(!group.containsKey(gb))
+    			group.put(gb, aValue);
+    		else
+    			group.put(gb, Math.max(aValue, group.get(gb)));
+    		break;
+    	case SUM:
+    		if(!group.containsKey(gb))
+    			group.put(gb, aValue);
+    		else
+    			group.put(gb, aValue+group.get(gb));
+    		break;
+    	case COUNT:
+    		if(!group.containsKey(gb))
+    			group.put(gb, 1);// 第一次添加
+    		else
+    			group.put(gb, 1+group.get(gb));// 加1
+    		break;
+    	case AVG:
+    		if(!group.containsKey(gb)) {
+    			group.put(gb, aValue);
+    			avgCount.put(gb, 1);// 用来记录该组有多少个值
+    		}
+    		else
+    		{
+    			int temp=avgCount.get(gb);
+    			avgCount.put(gb, temp+1);
+    			int avg=(temp*group.get(gb)+aValue)/(temp+1);// 求平均值
+    			group.put(gb, avg);
+    		}
+    		break;
+    	default:
+    		throw new IllegalArgumentException("Aggregate wrong!");
+    	}
     }
 
     /**
@@ -47,8 +130,70 @@ public class IntegerAggregator implements Aggregator {
      */
     public OpIterator iterator() {
         // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for lab2");
+    	
+        return new IntAggIterator();
     }
+    
+    private class IntAggIterator implements OpIterator{
+    	// 要返回tuple->tupleDesc
+    	private TupleDesc td;
+    	// 在group里面迭代
+    	private	Iterator<HashMap.Entry<Field,Integer>> it;
+    	
+    	public IntAggIterator(){
+    		// 构造tupleDesc->typeAr,fieldAr
+    		if(gbField==Aggregator.NO_GROUPING)
+    			td=new TupleDesc(new Type[] {Type.INT_TYPE},new String[] {"aggregateVal"});
+    		else 
+    			td=new TupleDesc(new Type[] {gbFieldType,Type.INT_TYPE},new String []{"groupVal","aggregateVal"});
+    	}
+    	
+		@Override
+		public void open() throws DbException, TransactionAbortedException {
+			// TODO Auto-generated method stub
+			it=group.entrySet().iterator();
+		}
+
+		@Override
+		public boolean hasNext() throws DbException, TransactionAbortedException {
+			// TODO Auto-generated method stub
+			return it.hasNext();
+		}
+
+		@Override
+		public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+			// TODO Auto-generated method stub
+			Map.Entry<Field, Integer> temp=it.next();
+			Tuple ret=new Tuple(td);
+			//做复杂了，又把gbField包装回了field :(
+			if(gbField==Aggregator.NO_GROUPING)// (aggregateVal)
+				ret.setField(0, new IntField(temp.getValue()));
+			else {// (groupVal,aggregateVal)
+				ret.setField(0, temp.getKey());
+				ret.setField(1, new IntField(temp.getValue()));
+			}
+			return ret;
+		}
+
+		@Override
+		public void rewind() throws DbException, TransactionAbortedException {
+			// TODO Auto-generated method stub
+			it=group.entrySet().iterator();
+		}
+
+		@Override
+		public TupleDesc getTupleDesc() {
+			// TODO Auto-generated method stub
+			return td;
+		}
+
+		@Override
+		public void close() {
+			// TODO Auto-generated method stub
+			it=null;
+		}
+    	
+    }
+    
 
 }
--- src/java/simpledb/StringAggregator.java
+++ src/java/simpledb/StringAggregator.java
@@ -1,12 +1,22 @@
 package simpledb;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
 /**
  * Knows how to compute some aggregate over a set of StringFields.
  */
 public class StringAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
-
+    private int gbField;
+    private Type gbFieldType;
+    private int aField;
+    private Op operator;
+    private HashMap<Field,Integer>group=new HashMap<>();// 干脆直接用field做key，反正最后转回tuple都得变成field
+    
     /**
      * Aggregate constructor
      * @param gbfield the 0-based index of the group-by field in the tuple, or NO_GROUPING if there is no grouping
@@ -18,14 +28,35 @@ public class StringAggregator implements Aggregator {
 
     public StringAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+    	this.gbField=gbfield;
+    	this.gbFieldType=gbfieldtype;
+    	this.aField=afield;
+    	this.operator=what;
+    	if(operator!=Op.COUNT)
+    		throw new IllegalArgumentException("wrong op!");
     }
 
     /**
      * Merge a new tuple into the aggregate, grouping as indicated in the constructor
      * @param tup the Tuple containing an aggregate field and a group-by field
      */
-    public void mergeTupleIntoGroup(Tuple tup) {
+    public void mergeTupleIntoGroup(Tuple tup) {// String只是afield是String？那有啥用呢？
         // some code goes here
+    	Field gb;
+
+    	if(this.gbField==Aggregator.NO_GROUPING)
+    		gb=null;
+    	else {
+    		gb=tup.getField(this.gbField);
+        	if(gbFieldType!=gb.getType())
+        		throw new IllegalArgumentException("wrong type!");
+    	}
+    	// 只做count！
+    	if(!group.containsKey(gb))
+    		group.put(gb, 1);
+    	else
+    		group.put(gb, group.get(gb)+1);
+    	
     }
 
     /**
@@ -38,7 +69,65 @@ public class StringAggregator implements Aggregator {
      */
     public OpIterator iterator() {
         // some code goes here
-        throw new UnsupportedOperationException("please implement me for lab2");
+        return new StrAggIterator();
+    }
+    
+    public class StrAggIterator implements OpIterator{
+    	private TupleDesc td;
+    	private	Iterator<HashMap.Entry<Field,Integer>> it;
+    	
+    	public StrAggIterator() {
+    		// 构造tupleDesc->typeAr,fieldAr
+    		if(gbField==Aggregator.NO_GROUPING)
+    			td=new TupleDesc(new Type[] {Type.INT_TYPE},new String[] {"aggregateVal"});
+    		else 
+    			td=new TupleDesc(new Type[] {gbFieldType,Type.INT_TYPE},new String []{"groupVal","aggregateVal"});
+    	}
+    	
+		@Override
+		public void open() throws DbException, TransactionAbortedException {
+			// TODO Auto-generated method stub
+			it=group.entrySet().iterator();
+		}
+
+		@Override
+		public boolean hasNext() throws DbException, TransactionAbortedException {
+			// TODO Auto-generated method stub
+			return it.hasNext();
+		}
+
+		@Override
+		public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+			// TODO Auto-generated method stub
+			Map.Entry<Field, Integer> temp=it.next();
+			Tuple ret=new Tuple(td);
+			if(gbField==Aggregator.NO_GROUPING)// (aggregateVal)
+				ret.setField(0, new IntField(temp.getValue()));
+			else {// (groupVal,aggregateVal)
+				ret.setField(0, temp.getKey());
+				ret.setField(1, new IntField(temp.getValue()));
+			}
+			return ret;
+		}
+
+		@Override
+		public void rewind() throws DbException, TransactionAbortedException {
+			// TODO Auto-generated method stub
+			it=group.entrySet().iterator();
+		}
+
+		@Override
+		public TupleDesc getTupleDesc() {
+			// TODO Auto-generated method stub
+			return td;
+		}
+
+		@Override
+		public void close() {
+			// TODO Auto-generated method stub
+			it=null;
+		}
+    	
     }
 
 }
